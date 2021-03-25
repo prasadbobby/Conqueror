287dac620016a1b30a4ba3696e6bebbc34a66ae4
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-25 11:42:14 +0530

    Update init.c

--- src/init.c
+++ src/init.c
@@ -1,5 +1,5 @@
-#include "syscall.h"
-#include "fcntl.h"
+#include <syscall.h>
+#include <fcntl.h>
 
 unsigned long _syscall(int num, void *a0, void *a1, void *a2, void *a3, void *a4, void *a5);
 unsigned long _strlen(char *sz)
