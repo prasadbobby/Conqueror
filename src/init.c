af94b2a57867bcd1714a6993092f99559d339776
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-25 11:07:43 +0530

    init.c

--- src/init.c
+++ src/init.c
@@ -1,6 +1,15 @@
 #include "syscall.h"
 
 long _syscall(int num, void *a0, void *a1, void *a2, void *a3, void *a4, void *a5);
+unsigned long _strlen(char *sz)
+{
+	int count = 0;
+	while(*sz++) {
+		count++;
+	}
+	return 0;
+}
+
 int main() 
 {
 	return 0;
