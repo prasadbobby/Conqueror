2608f7c762ec88394088f4480f7a5c5d18ecb831
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-26 17:29:01 +0530

    src

--- src/init/init.c
+++ src/init/init.c
@@ -1,4 +1,5 @@
 #include <syscall.h>
+#include <fcntl.h>
 
 unsigned long _syscall(int num, void *a0, void *a1, void *a2, void *a3, void *a4, void *a5);
 unsigned long _strlen(char *sz)
@@ -24,10 +25,32 @@ void print_string(char *str)
 	_syscall(SYS_write, (void *)1, str, (void *)_strlen(str), 0, 0, 0);
 }
 
+unsigned long open_file(char *fn, int flags)
+{
+	return _syscall(SYS_open, fn, flags, 0, 0, 0, 0);
+}
+
+unsigned long read_file(unsigned long fd, char *buff, unsigned long size)
+{
+	return _syscall(SYS_read, fd, buff, size, 0, 0, 0);
+}
+
 int main() 
 {
 	char *msg = "Conqueror_1.0 Initializing";
 	delay(100000000);
+	
+	print_string(msg);
+	delay(100000000);
+	char buff[255];
+	char *filename = "/src/init/init.c";
+	print_string("Opening file; ");
+	print_string(filename);
+	print_string("\n");
+	
+	unsigned long fd = open_file(filename, O_RDONLY);
+	read_file(fd, buff, sizeof(buff));
+	print_string(buff);
 	while(1)
 	{
 		delay(100000000);
