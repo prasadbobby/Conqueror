c5fecd68ee279b97a20fc8d0cb482f226c9b4775
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-27 12:49:25 +0530

    Update src

--- src/init/init.c
+++ src/init/init.c
@@ -25,16 +25,6 @@ void print_string(char *str)
 	_syscall(SYS_write, (void *)1, str, (void *)_strlen(str), 0, 0, 0);
 }
 
-unsigned long open_file(char *fn, int flags)
-{
-	return _syscall(SYS_open, fn, flags, 0, 0, 0, 0);
-}
-
-unsigned long read_file(unsigned long fd, char *buff, unsigned long size)
-{
-	return _syscall(SYS_read, fd, buff, size, 0, 0, 0);
-}
-
 int main() 
 {
 	char *msg = "Conqueror_1.0 Initializing";
