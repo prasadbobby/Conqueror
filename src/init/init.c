86212ac4a9089e4c97d3da33f54d0c81b209283c
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-27 12:53:16 +0530

    src

--- src/init/init.c
+++ src/init/init.c
@@ -1,29 +1,6 @@
 #include <syscall.h>
 #include <fcntl.h>
 
-unsigned long _strlen(char *sz)
-{
-	int count = 0;
-	while(*sz++)
-	{
-		count++;
-	}
-	return count;
-}
-
-void delay(int ticks)
-{
-	for(int i=;i<ticks;i++)
-	{
-		// nothing happpening
-	}
-}
-
-void print_string(char *str)
-{
-	_syscall(SYS_write, (void *)1, str, (void *)_strlen(str), 0, 0, 0);
-}
-
 int main() 
 {
 	char *msg = "Conqueror_1.0 Initializing";
