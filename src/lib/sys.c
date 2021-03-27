5acfe53f17b22eddb38f14011d5a7f06156e6660
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-27 12:50:19 +0530

    src

--- src/lib/sys.c
+++ src/lib/sys.c
@@ -1,1 +1,9 @@
+unsigned long open_file(char *fn, int flags)
+{
+	return _syscall(SYS_open, fn, flags, 0, 0, 0, 0);
+}
 
+unsigned long read_file(unsigned long fd, char *buff, unsigned long size)
+{
+	return _syscall(SYS_read, fd, buff, size, 0, 0, 0);
+}
