8cfac2ab571c588e34664547fe3b810f364109ec
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-27 13:04:47 +0530

    src\

--- src/lib/util.c
+++ src/lib/util.c
@@ -1,1 +1,24 @@
+#include <lib.h>
 
+unsigned str_len(char *sz)
+{
+	int count = 0;
+	while(*sz++)
+	{
+		count++;
+	}
+	return count;
+}
+
+void delay(int ticks)
+{
+	for(int i=;i<ticks;i++)
+	{
+		// nothing happpening
+	}
+}
+
+void str_print(char *str)
+{
+	_syscall(SYS_write, (void *)1, str, (void *)_strlen(str), 0, 0, 0);
+}
