910a1ffb093be1cfb57240a48cf62d82b5c5875e
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-27 12:51:19 +0530

    src

--- src/init/init.c
+++ src/init/init.c
@@ -1,7 +1,6 @@
 #include <syscall.h>
 #include <fcntl.h>
 
-unsigned long _syscall(int num, void *a0, void *a1, void *a2, void *a3, void *a4, void *a5);
 unsigned long _strlen(char *sz)
 {
 	int count = 0;
