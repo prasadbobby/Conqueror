f7b06fd76dd8a4602385fb7676c790fc2438d6a4
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-27 13:04:11 +0530

    src

--- src/lib/sys.c
+++ src/lib/sys.c
@@ -10,25 +10,4 @@ unsigned long sys_read(unsigned long fd, char *buff, unsigned long size)
 	return _syscall(SYS_read, fd, buff, size, 0, 0, 0);
 }
 
-unsigned str_len(char *sz)
-{
-	int count = 0;
-	while(*sz++)
-	{
-		count++;
-	}
-	return count;
-}
 
-void delay(int ticks)
-{
-	for(int i=;i<ticks;i++)
-	{
-		// nothing happpening
-	}
-}
-
-void str_print(char *str)
-{
-	_syscall(SYS_write, (void *)1, str, (void *)_strlen(str), 0, 0, 0);
-}
