ab8b2eaa4dd4100c269dc87221714f13d7238f8c
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-28 19:18:04 +0530

    Updated /src/lib/sys.c

--- src/lib/sys.c
+++ src/lib/sys.c
@@ -10,4 +10,10 @@ unsigned long sys_read(unsigned long fd, char *buff, unsigned long size)
 	return _syscall(SYS_read, fd, (void *)buff, (void *)size, 0, 0, 0);
 }
 
+unsigned long sys_reboot() 
+{
+	return _syscall(SYS_reboot, (void *)0xfee1dead, (void *)672274793, (void *)0x1234567, 0, 0, 0);
+}
+
+
 
