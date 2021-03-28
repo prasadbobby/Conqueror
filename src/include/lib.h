58a438cdbba634151771603597f24c761163ccd6
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-28 19:19:09 +0530

    Updated /src/include/lib.h

--- src/include/lib.h
+++ src/include/lib.h
@@ -4,6 +4,7 @@ extern unsigned long _syscall(int num, void *a0, void *a1, void *a2, void *a3, v
 
 extern unsigned long sys_open(char *fn, int flags);
 extern unsigned long sys_read(unsigned long fd, char *buff, unsigned long size);
+extern unsigned long sys_reboot();
 extern unsigned long str_len(char *sz);
 extern void str_print(char *str);
 extern void delay(int ticks);
