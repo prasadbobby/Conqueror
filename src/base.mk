116e9f46d810b86a4f829a8c4e6229fc718529d4
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-28 19:11:50 +0530

    Updated /src/base.mk

--- src/base.mk
+++ src/base.mk
@@ -4,9 +4,10 @@ FLAGS ?= -nostdlib -I../include
 MYOS_PATH ?= /mnt/Conqueror
 DESTDIR ?= /mnt/Conqueror
 
-
 PREFIX ?= $(DESTDIR)
 BINDIR ?= $(PREFIX)/sbin
+
+OBJECTS += ../crt0_s.o
 %_c.o : %.c
   $(CC) $(FLAGS) -c $< -o $@
   
