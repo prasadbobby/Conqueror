14f935811762246bada5316beb5790a432930cfe
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-28 18:46:23 +0530

    src

--- src/lib/sys.c
+++ src/lib/sys.c
@@ -2,12 +2,12 @@
 #include <lib.h>
 unsigned long sys_open(char *fn, int flags)
 {
-	return _syscall(SYS_open, fn, flags, 0, 0, 0, 0);
+	return _syscall(SYS_open, fn, (void *)(long)flags, 0, 0, 0, 0);
 }
 
 unsigned long sys_read(unsigned long fd, char *buff, unsigned long size)
 {
-	return _syscall(SYS_read, fd, buff, size, 0, 0, 0);
+	return _syscall(SYS_read, fd, (void *)buff, (void *)size, 0, 0, 0);
 }
 
 
