a21864e605ca7091869f36bf07a7c8be2e2dcfac
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-29 11:23:35 +0530

    Updated /src/lash/lash.c

--- src/lash/lash.c
+++ src/lash/lash.c
@@ -7,3 +7,16 @@ void console_open()
 {
   console_fd = sys_open("/dev/console/", O_RDWR | O_NDELAY);
 }
+
+char console_read()
+{
+  char c = 0;
+  while(!sys_read(console_fd, &c, 1))
+  {
+    struct timespec ts;
+    ts.tv_sec = 0;
+    ts.tv_nsec = 10000;
+    sys_nanosleep(&ts, NULL);
+  }
+  retun c;
+}
