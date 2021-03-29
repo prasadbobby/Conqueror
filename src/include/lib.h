c9899409911f84b25960bc266be0fffd5e51ed09
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-29 11:13:09 +0530

    Updated /src/lib/lib.h

--- src/include/lib.h
+++ src/include/lib.h
@@ -5,6 +5,7 @@ extern unsigned long _syscall(int num, void *a0, void *a1, void *a2, void *a3, v
 extern unsigned long sys_open(char *fn, int flags);
 extern unsigned long sys_read(unsigned long fd, char *buff, unsigned long size);
 extern unsigned long sys_reboot();
+unsigned long sys_nanosleep(struct timespec *req, struct timespec *rem);
 extern unsigned long str_len(char *sz);
 extern void str_print(char *str);
 extern void delay(int ticks);
