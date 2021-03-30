e1bf3283d582826fb0522023ce2811b856b17b6f
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-03-30 12:30:52 +0530

    Updated /src/lib/util.c

--- src/lib/util.c
+++ src/lib/util.c
@@ -23,3 +23,19 @@ void str_print(char *str)
 {
 	_syscall(SYS_write, (void *)1, str, (void *)str_len(str), 0, 0, 0);
 }
+
+int str_eq(char *a, char *b)
+{
+	char ca = *a;
+	char cb = *b;
+	
+	for(; ca != 0 && cb != 0; ca = *(++a), cb = *(++b))
+	{
+		if (ca != cb)
+		{
+			return 0;
+		}
+	}
+	return ca == cb;
+}
+
