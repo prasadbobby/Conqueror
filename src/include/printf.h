99ec765ebe9b5def2e30fe054989430d6088955f
mahendra gandham <80026278+mahendragandham@users.noreply.github.com>
2021-04-02 19:34:38 +0530

    Updated /src/include/printf.h

--- src/include/printf.h
+++ src/include/printf.h
@@ -7,3 +7,4 @@
 void init_printf(void* putp,void (*putf) (void*,char));
 void tfp_printf(char *fmt, ...);
 void tfp_sprintf(char* s,char *fmt, ...);
+void tfp_format(void* putp,void (*putf) (void*,char),char *fmt, va_list va);
